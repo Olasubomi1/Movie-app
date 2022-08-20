a5726ca232a4191d507afb362200793da9464a5f
Olasubomi <shubbynlaa@gmail.com>
2022-08-20 02:49:05 +0100

    set 'token' into a constant tokenKey.

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,32 +3,33 @@ import http from "./httpService";
 import { apiUrl } from "../config.json";
 
 const apiEndpoint = apiUrl + "/auth";
+const tokenKey = "token";
 
 http.setJwt(getJwt());
 
 export async function login(email, password) {
   const { data: jwt } = await http.post(apiEndpoint, { email, password });
-  localStorage.setItem("token", jwt);
+  localStorage.setItem(tokenKey, jwt);
 }
 
 export function loginWithJwt(jwt) {
-  localStorage.setItem("token", jwt);
+  localStorage.setItem(tokenKey, jwt);
 }
 
 export function logout() {
-  localStorage.removeItem("token");
+  localStorage.removeItem(tokenKey);
 }
 
 export function getCurrentUser() {
   try {
-    const jwt = localStorage.getItem("token");
+    const jwt = localStorage.getItem(tokenKey);
     return jwtDecode(jwt);
   } catch (error) {
     return null;
   }
 }
 export function getJwt() {
-  return localStorage.getItem("token");
+  return localStorage.getItem(tokenKey);
 }
 
 export default {
